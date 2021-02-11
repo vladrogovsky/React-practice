0105000768a2e67bf059e210c14e30690bd1a199
Vladislav Rogovsky <vladislavrogovsky@gmail.com>
2021-02-11 23:28:50 +0200

    modal & useReduce

--- P1/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ P1/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainter = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainter.current.value);
+  };
+  useEffect(() => {
+    console.log(refContainter.current);
+    refContainter.current.focus();
+  });
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" name="" id="" ref={refContainter} />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
--- P1/src/tutorial/6-useReducer/setup/index.js
+++ P1/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,44 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [people, setpeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setShowModal(true);
+      setpeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName("");
+    } else {
+      setShowModal(false);
+    }
+  };
+  return (
+    <>
+      {showModal && <Modal></Modal>}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">add person</button>
+        </div>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
